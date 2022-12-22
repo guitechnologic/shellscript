ca087c371e2cf214d2a43bc0c68d01cfed9f0094
Guilherme goulart <gui.technologic@gmail.com>
2022-12-22 16:00:24 -0300

    chmod 777 geral

--- shell-script-do-basico-ao-profissional/lacos.sh
+++ shell-script-do-basico-ao-profissional/lacos.sh
@@ -1,9 +1,46 @@
 #!/usr/bin/env bash
 
 echo "=== for 1"
-for (( i = 0; i < 10; i++)); do
+    for (( i = 0; i < 10; i++)); do
 echo $i
 done
+
+echo "-----------------------------"
+
+echo "=== for 1"
+    for i in $(seq 10);
+do 
+    echo $i
+done
+
+echo "-------array----------------------"
+Frutas=(
+    'laranja'
+    'ameixa'
+    'abacaxi'
+    'melancia'
+)
+for i in "${Frutas[@]}"; do
+echo $i
+done
+
+
+echo "-------while----------------------"
+contador=0
+while [[ $contador -lt  ${#Frutas[@]} ]]; do
+echo $contador
+contador=$(($contador+1))
+done
+
+
+
+
+
+
+
+
+
+
 
 
 
